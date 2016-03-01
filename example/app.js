970a0d8f29196ffd0071840dd05f8ece6d15c28d
stonexer <shitianxin@gmail.com>
2016-03-01 11:33:50 +0800

    fix ignore bug

---
+++ example/app.js
@@ -0,0 +1,22 @@
+"use strict"
+const express = require('express')
+const wechat = require('../index')
+const debug = require('debug')('app')
+
+const api = require('./routes/api')
+
+const app = express()
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// 静态文件
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('layout')
+})
+
+app.use('/api', api)
+
+const server = app.listen(3000)
