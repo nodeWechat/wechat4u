30e1c1512b52ee62ca80b87f461074a430a863a7
stonexer <shitianxin@gmail.com>
2016-03-04 09:19:00 +0800

    ignore fix & add example/lib

---
+++ example/lib/wxbot.js
@@ -0,0 +1,57 @@
+"use strict"
+const Wechat = require('../../index')
+const debug = require('debug')('wxbot')
+
+class WxBot extends Wechat {
+
+  constructor() {
+    super()
+
+    this.replyUsers = new Set()
+    this.on('text-message', msg => this._botReply(msg))
+
+    this.superviseUsers = new Set()
+    this.openTimes = 0
+    this.on('mobile-open', () => this._botSupervise())
+  }
+
+  _tuning(word) {
+    let params = {
+      'key': '2ba083ae9f0016664dfb7ed80ba4ffa0',
+      'info': word
+    }
+    return this.axios({
+      method: 'GET',
+      url: 'http://www.tuling123.com/openapi/api',
+      params: params
+    }).then(res => {
+      const data = res.data
+      if (data.code == 100000) {
+        return data.text + '[微信机器人]'
+      }      throw new Error("tuning返回值code错误", data)
+    }).catch(err => {
+      debug(err)
+      return "现在思路很乱，最好联系下我哥 T_T..."
+    })
+  }
+
+  _botReply(msg) {
+    debug('自动回复')
+    if (this.replyUsers.has(msg['FromUserName'])) {
+      this._tuning(msg['Content']).then((reply) => {
+        this.sendMsg(reply, msg['FromUserName'])
+        debug(reply)
+      })
+    }
+  }
+
+  _botSupervise() {
+    const message = '我的主人玩微信' + ++this.openTimes + '次啦！'
+    for (let user in this.superviseUsers) {
+      bot.sendMsg(message, user)
+    }
+  }
+
+}
+
+exports = module.exports = WxBot
