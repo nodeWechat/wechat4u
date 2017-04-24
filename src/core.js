84a83319418bb127d75e0d446f6011ede83d5efb
mgampkay <mgampkay@gmail.com>
2017-04-24 18:56:44 +0800

    优化异常处理

--- src/core.js
+++ src/core.js
@@ -922,15 +922,15 @@ export default class WechatCore {
   }
 
   getDoc (FromUserName, MediaId, FileName) {
-    let params = {
-      sender: FromUserName,
-      mediaid: MediaId,
-      filename: FileName,
-      fromuser: this.user.UserName,
-      pass_ticket: this.PROP.passTicket,
-      webwx_data_ticket: this.PROP.webwxDataTicket
-    }
     return Promise.resolve().then(() => {
+      let params = {
+        sender: FromUserName,
+        mediaid: MediaId,
+        filename: FileName,
+        fromuser: this.user.UserName,
+        pass_ticket: this.PROP.passTicket,
+        webwx_data_ticket: this.PROP.webwxDataTicket
+      }
       return this.request({
         method: 'GET',
         url: this.CONF.API_webwxdownloadmedia,
@@ -945,7 +945,7 @@ export default class WechatCore {
     }).catch(err => {
       debug(err)
       err.tips = '获取文件失败'
-      throw new Error('获取文件失败')
+      throw err;
     })
   }
 
